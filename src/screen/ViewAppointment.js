d61c8f928fa896c23865ddbe23c0797c4003ba26
danial <danialarrifin11@gmail.com>
2023-05-19 16:21:23 +0800

    several changes regarding icons, buttons added

--- src/screen/ViewAppointment.js
+++ src/screen/ViewAppointment.js
@@ -7,16 +7,16 @@ import AntDesgin from 'react-native-vector-icons/AntDesign'
 function ViewAppointment({ navigation }) {
     return (
         <>
-        <View style={{ backgroundColor: 'firebrick',  borderBottomEndRadius: 4, height: '10%' }} >
+            <View style={{ backgroundColor: 'firebrick', borderBottomEndRadius: 4, height: '10%' }} >
                 <View style={{ flexDirection: 'row', marginVertical: 5 }}>
                     <AntDesgin name='arrowleft' style={{ color: 'white', fontSize: 20, marginTop: 20, marginLeft: 10 }} onPress={() => navigation.navigate(PATH_HOME.myworkshop)} />
-                    <Text style={{marginTop: 18, fontSize: 15, marginLeft: 100, color: 'white'}}> View Appointments</Text>
+                    <Text style={{ marginTop: 18, fontSize: 15, marginLeft: 100, color: 'white' }}> View Appointments</Text>
                 </View>
             </View>
-            <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: 30 }}>
-                <Text style={{ fontSize: 20, marginBottom: 10, color: 'firebrick', fontFamily: 'Roboto-Bold' }}  onPress={() => navigation.navigate(PATH_HOME.dashboard)}> View Appointments</Text>
+            <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: 10 }}>
+                <Text style={{ fontSize: 20, marginBottom: 10, color: 'firebrick', fontFamily: 'Roboto-Bold' }} onPress={() => navigation.navigate(PATH_HOME.dashboard)}> View Appointments</Text>
             </View>
-            <View style={{ backgroundColor: 'firebrick', alignItems: 'center', borderRadius: 30, height: '30%', marginLeft: 30, marginBottom: 10, elevation: 10, justifyContent: 'center', width: '85%' }}>
+            <View style={{ backgroundColor: 'firebrick', alignItems: 'center', borderRadius: 30, height: '35%', marginLeft: 30, marginBottom: 10, elevation: 10, justifyContent: 'center', width: '85%' }}>
                 <Text style={{ fontSize: 20, marginBottom: 5, color: 'white' }}>Pohleh Workshop Sdn Bhd</Text>
                 <View style={{ flexDirection: 'row', marginVertical: 5 }}>
                     <AntDesgin name='calendar' style={{ color: 'white', fontSize: 20, marginTop: 5 }} />
@@ -34,9 +34,29 @@ function ViewAppointment({ navigation }) {
                     <Text style={{ fontSize: 20, marginBottom: 5, color: 'white', marginLeft: 20 }}>Status:</Text>
                     <Text style={{ fontSize: 20, marginBottom: 5, color: 'white', marginLeft: 20, fontFamily: 'Roboto-Bold' }}>Accepted</Text>
                 </View>
+                <View>
+                    
+                </View>
+                <View style={{ width: '30%', height: '15%', flexDirection: 'row', marginVertical: 5, justifyContent: 'center'}}>
+                    <Button
+                        color={'grey'}
+                        title="Done"
+                        onPress={() => navigation.navigate(PATH_HOME.completedappointment)}
+                   
+                    />
+                    <Button
+                        color={'lightcoral'}
+                        title="Reject"
+                    />
+                     <Button
+                        color={'green'}
+                        title="Accept"
+                   
+                    />
+                </View>
             </View>
 
-            <View style={{ backgroundColor: 'firebrick', alignItems: 'center', borderRadius: 30, height: '30%', marginLeft: 30, marginBottom: 10, elevation: 10, justifyContent: 'center', width: '85%' }}>
+            <View style={{ backgroundColor: 'firebrick', alignItems: 'center', borderRadius: 30, height: '35%', marginLeft: 30, elevation: 10, justifyContent: 'center', width: '85%' }}>
                 <Text style={{ fontSize: 20, marginBottom: 5, color: 'white' }}>Serv Malaysia</Text>
                 <View style={{ flexDirection: 'row', marginVertical: 5 }}>
                     <AntDesgin name='calendar' style={{ color: 'white', fontSize: 20, marginTop: 5 }} />
@@ -52,15 +72,33 @@ function ViewAppointment({ navigation }) {
                 </View>
                 <View style={{ flexDirection: 'row', marginVertical: 5, }}>
                     <Text style={{ fontSize: 20, marginBottom: 5, color: 'white', marginLeft: 20 }}>Status:</Text>
-                    <Text style={{ fontSize: 20, marginBottom: 5, color: 'white', marginLeft: 20, fontFamily: 'Roboto-BoldItalic' }}>Completed</Text>
+                    <Text style={{ fontSize: 20, marginBottom: 5, color: 'white', marginLeft: 20, fontFamily: 'Roboto-Bold' }}>Rejected</Text>
                 </View>
+                <View style={{ width: '30%', height: '15%', flexDirection: 'row', marginVertical: 5, justifyContent:'center' }}>
+                <Button
+                        color={'grey'}
+                        title="Done"
+                        onPress={() => navigation.navigate(PATH_HOME.completedappointment)}
+                   
+                    />
+                    <Button
+                        color={'green'}
+                        title="Accept"
+                     
+                    />
+                    <Button
+                        color={'lightcoral'}
+                        title="Reject"
+                    />
+                </View>
+
             </View>
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <View style={{ width: '80%', height: '60%', }}>
                     <Button
                         color={'#b22222'}
-                        title="Cancel Appointments"
-                    // onPress={() => navigation.navigate(PATH_HOME.cancelappointment)}
+                        title="Completed Appointments"
+                    onPress={() => navigation.navigate(PATH_HOME.completedappointment)}
                     />
                 </View>
             </View>
