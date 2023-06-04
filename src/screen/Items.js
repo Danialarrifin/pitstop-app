c8669749e83056127f24963603c201243930dff8
danial <danialarrifin11@gmail.com>
2023-06-04 16:22:28 +0800

    table testing for services

--- src/screen/Items.js
+++ src/screen/Items.js
@@ -1,6 +1,12 @@
 import { Button, View, Text } from 'react-native';
 import { PATH_AUTH, PATH_HOME } from '../navigations/path';
 import AntDesgin from 'react-native-vector-icons/AntDesign'
+import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
+
+const headers =['Header 1', 'Header 2', 'Header 3', 'Header 4']
+const rows= [
+    ['Row 11', 'Row 11', 'Row 11', 'Row 11']
+]
 
 function Items({ navigation }) {
     return (
@@ -10,6 +16,26 @@ function Items({ navigation }) {
                     <AntDesgin name='arrowleft' style={{ color: 'white', fontSize: 20, marginTop: 20, marginLeft: 10 }} onPress={() => navigation.navigate(PATH_HOME.viewworkshop)}/>
                     <Text style={{marginTop: 18, fontSize: 15, marginLeft: 140, color: 'white'}}> Services</Text>
                 </View>
+            </View>
+            <View style={{flex: 1, padding: 10}}>
+                <Table borderStyle={{borderWidth: 1}}>
+                    <Row
+                    data={headers}
+                    style={{
+                        backgroundColor:'firebrick'
+                    }}
+                    height={40}
+                    flexArr={[1,1,1,1]}
+                    textStyle={{
+                        textAlign: 'center'
+                    }}
+                    />
+                    <TableWrapper>
+                        <Rows data={rows} heightArr={[28, 28, 28, 28]} flexArr={[1,1,1,1]} textStyle={{
+                            textAlign: 'center'
+                        }} />
+                    </TableWrapper>
+                </Table>
             </View>
         </>
     );
