2fe5a407ee6eee7fa5cb2b08beac406229fbf69f
danial <danialarrifin11@gmail.com>
2023-06-07 16:45:46 +0800

    transaction history integrate

--- src/screen/History.js
+++ src/screen/History.js
@@ -1,9 +1,34 @@
 import { Button, View, Text } from 'react-native';
 import { PATH_AUTH, PATH_HOME } from '../navigations/path';
 import AntDesgin from 'react-native-vector-icons/AntDesign'
+import React, { useEffect, useState } from 'react';
+import axiosInstance from '../utils/axios';
 
 
 function History({ navigation }) {
+const [transaction, setTransaction] = useState([]);
+
+  useEffect(() => {
+    getAllTransaction();
+  });
+
+  const getAllTransaction = async () => {
+    try {
+      const response = await axiosInstance.get(
+        "/transactions",
+        {
+          headers: {
+            'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZW1haWwiOiJhZG1pbjNAdGVzdC5jb20iLCJpYXQiOjE2ODYxMTk0NjUsImV4cCI6MTY4NjIwNTg2NX0.OLaKGGIy0M9uOiYq3m2aO3-cB0FR7h4TqqLny9biYfU`
+          }
+        }
+      );
+      console.log('response vehicle', response.data);
+      if (response.data.length > 0)
+        setTransaction(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
   return (
     <>
       <View style={{ backgroundColor: 'firebrick', borderBottomEndRadius: 4, height: '10%' }} >
@@ -15,37 +40,29 @@ function History({ navigation }) {
       <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: 30 }}>
         <Text style={{ fontSize: 20, marginBottom: 10, color: 'firebrick', fontFamily: 'Roboto-Bold' }}>Transaction History</Text>
       </View>
-      <View style={{ backgroundColor: 'firebrick', alignItems: 'center', borderRadius: 30, height: '30%', marginLeft: 30, marginBottom: 10, elevation: 10, justifyContent: 'center', width: '85%' }}>
-        <Text style={{ fontSize: 20, marginBottom: 5, color: 'white' }}>Pohleh Workshop Sdn Bhd</Text>
-        <View style={{ flexDirection: 'row', marginVertical: 5 }}>
-          <AntDesgin name='calendar' style={{ color: 'white', fontSize: 20, marginTop: 5 }} />
-          <Text style={{ fontSize: 20, marginBottom: 5, color: 'white', marginLeft: 20 }}>29/11/2022</Text>
-        </View>
-        <View style={{ flexDirection: 'row', marginVertical: 5, }}>
-          <AntDesgin name='car' style={{ color: 'white', fontSize: 20, marginTop: 5 }} />
-          <Text style={{ fontSize: 20, marginBottom: 5, color: 'white', marginLeft: 20 }}>Brake Pad Change and Flat Tyre</Text>
-        </View>
-        <View style={{ flexDirection: 'row', marginVertical: 5, }}>
-          <AntDesgin name='pay-circle-o1' style={{ color: 'white', fontSize: 20, marginTop: 5 }} />
-          <Text style={{ fontSize: 20, marginBottom: 5, color: 'white', marginLeft: 20 }}>RM350.00</Text>
-        </View>
-      </View>
+      {transaction.length > 0 ?
+        transaction.map((item) => {
+          return (
+            <>
+              <View style={{ backgroundColor: 'firebrick', alignItems: 'center', borderRadius: 30, height: '30%', marginLeft: 30, marginBottom: 10, elevation: 10, justifyContent: 'center', width: '85%' }}>
+                <Text style={{ fontSize: 20, marginBottom: 5, color: 'white' }}>Pohleh Workshop Sdn Bhd</Text>
+                <View style={{ flexDirection: 'row', marginVertical: 5 }}>
+                  <AntDesgin name='calendar' style={{ color: 'white', fontSize: 20, marginTop: 5 }} />
+                  <Text style={{ fontSize: 20, marginBottom: 5, color: 'white', marginLeft: 20 }}>{item.appointment_id}</Text>
+                </View>
+                <View style={{ flexDirection: 'row', marginVertical: 5, }}>
+                  <AntDesgin name='car' style={{ color: 'white', fontSize: 20, marginTop: 5 }} />
+                  <Text style={{ fontSize: 20, marginBottom: 5, color: 'white', marginLeft: 20 }}>{item.remarks}</Text>
+                </View>
+                <View style={{ flexDirection: 'row', marginVertical: 5, }}>
+                  <AntDesgin name='pay-circle-o1' style={{ color: 'white', fontSize: 20, marginTop: 5 }} />
+                  <Text style={{ fontSize: 20, marginBottom: 5, color: 'white', marginLeft: 20 }}>{item.final_price}</Text>
+                </View>
+              </View>
+            </>
+          )
+        }) : null}
 
-      <View style={{ backgroundColor: 'firebrick', alignItems: 'center', borderRadius: 30, height: '30%', marginLeft: 30, marginBottom: 10, elevation: 10, justifyContent: 'center', width: '85%' }}>
-        <Text style={{ fontSize: 20, marginBottom: 5, color: 'white' }}>Serv Malaysia</Text>
-        <View style={{ flexDirection: 'row', marginVertical: 5 }}>
-          <AntDesgin name='calendar' style={{ color: 'white', fontSize: 20, marginTop: 5 }} />
-          <Text style={{ fontSize: 20, marginBottom: 5, color: 'white', marginLeft: 20 }}>4/11/2022</Text>
-        </View>
-        <View style={{ flexDirection: 'row', marginVertical: 5, }}>
-          <AntDesgin name='car' style={{ color: 'white', fontSize: 20, marginTop: 5 }} />
-          <Text style={{ fontSize: 20, marginBottom: 5, color: 'white', marginLeft: 20 }}>Engine Overhaul</Text>
-        </View>
-        <View style={{ flexDirection: 'row', marginVertical: 5, }}>
-          <AntDesgin name='pay-circle-o1' style={{ color: 'white', fontSize: 20, marginTop: 5 }} />
-          <Text style={{ fontSize: 20, marginBottom: 5, color: 'white', marginLeft: 20 }}>RM1000.00</Text>
-        </View>
-      </View>
     </>
   );
 }
