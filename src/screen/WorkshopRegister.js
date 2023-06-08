723d79cccaa8141c0595f7be574ccd845e0be4d3
danial <danialarrifin11@gmail.com>
2023-06-08 20:54:51 +0800

    workshop register updated fe

--- src/screen/WorkshopRegister.js
+++ src/screen/WorkshopRegister.js
@@ -1,31 +1,48 @@
-import { Button, View, Text, TextInput } from 'react-native';
+import { Button, View, Text, TextInput, ScrollView } from 'react-native';
 import { PATH_AUTH, PATH_HOME } from '../navigations/path';
 function WorkshopRegister({ navigation }) {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <View style={{ width: '80%', height: '60%' }}>
-                <Text style={{color:'firebrick'}}>Name</Text>
-                <TextInput style={{ height: 50, width: 'auto', marginBottom: 15, borderColor: 'black', borderWidth: 1, backgroundColor:'white', borderRadius: 4  }} />
-
-                <Text style={{color:'firebrick'}}>Phone Number</Text>
-                <TextInput style={{ height: 50, width: 'auto', marginBottom: 15, borderColor: 'black', borderWidth: 1, backgroundColor:'white', borderRadius: 4  }} />
-
-                <Text style={{color:'firebrick'}}>Email address</Text>
-                <TextInput style={{ height: 50, width: 'auto', marginBottom: 15, borderColor: 'black', borderWidth: 1, backgroundColor:'white', borderRadius: 4  }} />
-
-                <Text style={{color:'firebrick'}}> Address</Text>
-                <TextInput style={{ height: 50, width: 'auto', marginBottom: 15, borderColor: 'black', borderWidth: 1, backgroundColor:'white', borderRadius: 4  }} />
-
-                <Text style={{color:'firebrick'}}>Password</Text>
-                <TextInput style={{ height: 50, width: 'auto', marginBottom: 15, borderColor: 'black', borderWidth: 1, backgroundColor:'white', borderRadius: 4  }} />
-
+        <ScrollView >
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <View style={{ width: '80%' }}>
+                    <Text style={{ color: 'firebrick' }}>Name</Text>
+                    <TextInput style={{ height: 50, width: 'auto', marginBottom: 15, borderColor: 'black', borderWidth: 1, backgroundColor: 'white', borderRadius: 4 }} />
+                </View>
+                <View style={{ width: '80%' }}>
+                    <Text style={{ color: 'firebrick' }}>Phone Number</Text>
+                    <TextInput style={{ height: 50, width: 'auto', marginBottom: 15, borderColor: 'black', borderWidth: 1, backgroundColor: 'white', borderRadius: 4 }} />
+                </View>
+                <View style={{ width: '80%' }}>
+                    <Text style={{ color: 'firebrick' }}>Email Address</Text>
+                    <TextInput style={{ height: 50, width: 'auto', marginBottom: 15, borderColor: 'black', borderWidth: 1, backgroundColor: 'white', borderRadius: 4 }} />
+                </View>
+                <View style={{ width: '80%' }}>
+                    <Text style={{ color: 'firebrick' }}> Address</Text>
+                    <TextInput style={{ height: 50, width: 'auto', marginBottom: 15, borderColor: 'black', borderWidth: 1, backgroundColor: 'white', borderRadius: 4 }} />
+                </View>
+                <View style={{ width: '80%' }}>
+                    <Text style={{ color: 'firebrick' }}> State</Text>
+                    <TextInput style={{ height: 50, width: 'auto', marginBottom: 15, borderColor: 'black', borderWidth: 1, backgroundColor: 'white', borderRadius: 4 }} />
+                </View>
+                <View style={{ width: '80%' }}>
+                    <Text style={{ color: 'firebrick' }}> Postcode</Text>
+                    <TextInput style={{ height: 50, width: 'auto', marginBottom: 15, borderColor: 'black', borderWidth: 1, backgroundColor: 'white', borderRadius: 4 }} />
+                </View>
+                <View style={{ width: '80%' }}>
+                    <Text style={{ color: 'firebrick' }}> City</Text>
+                    <TextInput style={{ height: 50, width: 'auto', marginBottom: 15, borderColor: 'black', borderWidth: 1, backgroundColor: 'white', borderRadius: 4 }} />
+                </View>
+                <View style={{ width: '80%' }}>
+                    <Text style={{ color: 'firebrick' }}> Password</Text>
+                    <TextInput style={{ height: 50, width: 'auto', marginBottom: 15, borderColor: 'black', borderWidth: 1, backgroundColor: 'white', borderRadius: 4 }} />
+                </View>
                 <Button
                     color={'#b22222'}
                     title="Sign Me Up"
                     onPress={() => navigation.navigate(PATH_HOME.myworkshop)}
                 />
             </View>
-        </View>
+        </ScrollView>
     );
 }
 
