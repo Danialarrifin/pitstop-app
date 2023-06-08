03170dd89ac4ab803b268bfaaeaba40065134ed4
danial <danialarrifin11@gmail.com>
2023-06-08 23:10:28 +0800

    auth provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Navigation from './navigations/main' 
+import Navigation from './navigations/main'
+import { AuthProvider } from './context/AuthContext';
 
-function App() {
+const App = () => {
   return (
-    <Navigation/>
+    <AuthProvider>
+      <Navigation />
+    </AuthProvider>
   );
 }
 
