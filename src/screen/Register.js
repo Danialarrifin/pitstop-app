f5b629cf867935b023072ba7894d9a8af9eb8c1f
danial <danialarrifin11@gmail.com>
2023-06-12 01:51:01 +0800

    tried to axios post, didnt workout

--- src/screen/Register.js
+++ src/screen/Register.js
@@ -1,27 +1,43 @@
-import { Button, View, Text, TextInput } from 'react-native';
+import React, {useContext, useState} from 'react';
+import {AuthContext} from '../context/AuthContext';
+import { Button, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import { PATH_AUTH, PATH_HOME } from '../navigations/path';
 function Register({ navigation }) {
+
+    const [name, setName] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+  
+    const {register} = useContext(AuthContext);
+
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <View style={{ width: '80%', height: '60%' }}>
                 <Text style={{color:'firebrick'}}>Name</Text>
-                <TextInput style={{ height: 50, width: 'auto', marginBottom: 15, borderColor: 'black', borderWidth: 1, backgroundColor:'white', borderRadius: 4  }} />
-
-                <Text style={{color:'firebrick'}}>Phone Number</Text>
-                <TextInput style={{ height: 50, width: 'auto', marginBottom: 15, borderColor: 'black', borderWidth: 1, backgroundColor:'white', borderRadius: 4  }} />
+                <TextInput style={{ height: 50, width: 'auto', marginBottom: 15, borderColor: 'black', borderWidth: 1, backgroundColor:'white', borderRadius: 4}} value={name}
+ onChangeText={text => setName(text)}/>
 
                 <Text style={{color:'firebrick'}}>Email address</Text>
-                <TextInput style={{ height: 50, width: 'auto', marginBottom: 15, borderColor: 'black', borderWidth: 1, backgroundColor:'white', borderRadius: 4  }} />
+                <TextInput style={{ height: 50, width: 'auto', marginBottom: 15, borderColor: 'black', borderWidth: 1, backgroundColor:'white', borderRadius: 4}} value={email}
+                 onChangeText={text => setEmail(text)} />
 
                 <Text style={{color:'firebrick'}}>Password</Text>
-                <TextInput style={{ height: 50, width: 'auto', marginBottom: 15, borderColor: 'black', borderWidth: 1, backgroundColor:'white', borderRadius: 4  }} />
+                <TextInput style={{ height: 50, width: 'auto', marginBottom: 15, borderColor: 'black', borderWidth: 1, backgroundColor:'white', borderRadius: 4}}   value={password} 
+                onChangeText={text => setPassword(text)}/>
 
                 <Button
                     color={'#b22222'}
                     title="Sign Me Up"
-                    onPress={() => navigation.navigate(PATH_AUTH.login)}
+                    onPress={() => {
+                        register(name, email, password);
+                      }}
                 />
+                <View style={{ flexDirection: 'row', justifyContent: 'center', alignContent: 'center', marginTop: 10 }}>
+                    <Text>Already have an account? </Text>
+                    <TouchableOpacity onPress={() => navigation.navigate(PATH_AUTH.login)}><Text>Login Here</Text></TouchableOpacity>
+                </View>
             </View>
+            
         </View>
     );
 }
