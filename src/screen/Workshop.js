264a5c5b9ddd5f47615f33662979188d46fee211
danial <danialarrifin11@gmail.com>
2023-06-12 22:52:47 +0800

    flatlist and also navigate viewwrokshop by id

--- src/screen/Workshop.js
+++ src/screen/Workshop.js
@@ -1,9 +1,10 @@
-import { Button, View, Text, TextInput, Image, ScrollView } from 'react-native';
+import { Button, View, Text, TextInput, Image, SafeAreaView, FlatList } from 'react-native';
 import { PATH_AUTH, PATH_HOME } from '../navigations/path';
 import AntDesgin from 'react-native-vector-icons/AntDesign'
 import axiosInstance from '../utils/axios';
 import React, { useContext, useState, useEffect } from 'react';
 import { AuthContext } from '../context/AuthContext';
+
 
 
 
@@ -34,24 +35,48 @@ function Workshop({ navigation }) {
     }
   }
   return (
-    <>
+    // <ScrollView>
+    //   <View style={{ backgroundColor: 'firebrick', borderBottomEndRadius: 4, height: '20%' }} >
+    //     <View style={{ flexDirection: 'row', marginVertical: 5 }}>
+    //       <AntDesgin name='arrowleft' style={{ color: 'white', fontSize: 20, marginTop: 20, marginLeft: 10 }} onPress={() => navigation.navigate(PATH_HOME.dashboard)} />
+    //       <Text style={{ marginTop: 18, fontSize: 15, marginLeft: 140, color: 'white' }}> Workshops</Text>
+    //     </View>
+    //   </View>
+    //   <View style={{ alignItems: 'center', justifyContent: 'center', marginBottom: 20 }}>
+    //     {workshop.length > 0 ?
+    //       workshop.map((item) => {
+    //         return (
+    //           // <View style={{ backgroundColor: 'firebrick', alignItems: 'center', borderRadius: 50, marginHorizontal: 40, marginVertical: 10, elevation: 10, justifyContent: 'center', height: '20%', width: '80%' }}>
+    //           //   <View style={{ backgroundColor: 'white', justifyContent: 'center', alignItems: 'center', borderRadius: 30, elevation: 10, width: '100%', height: '100%' }}>
+    //           //     <Text style={{ color: 'black' }} onPress={() => navigation.navigate(PATH_HOME.viewworkshop)}>{item.name}</Text>
+    //           //   </View>
+    //           // </View>
+    //           <View style={{ backgroundColor: 'firebrick', alignItems: 'center', borderRadius: 50, marginHorizontal: 40, marginVertical: 10, elevation: 10, justifyContent: 'center', height: '20%', width: '80%' }}>
+    //             <Text style={{ color: 'black' }} onPress={() => navigation.navigate(PATH_HOME.viewworkshop, { workshopId: item?.id })}>{item.name}</Text>
+    //           </View>
+    //         )
+    //       }) : null}
+
+    //   </View>
+    // </ScrollView>
+    <SafeAreaView style={{ flex: 1 }}>
       <View style={{ backgroundColor: 'firebrick', borderBottomEndRadius: 4, height: '10%' }} >
         <View style={{ flexDirection: 'row', marginVertical: 5 }}>
           <AntDesgin name='arrowleft' style={{ color: 'white', fontSize: 20, marginTop: 20, marginLeft: 10 }} onPress={() => navigation.navigate(PATH_HOME.dashboard)} />
           <Text style={{ marginTop: 18, fontSize: 15, marginLeft: 140, color: 'white' }}> Workshops</Text>
         </View>
       </View>
-      {workshop.length > 0 ?
-        workshop.map((item) => {
-          return (
-                <View style={{ backgroundColor: 'firebrick', alignItems: 'center', borderRadius: 50, height: '18%', marginLeft: 40, marginRight: 40, marginBottom: 10, marginTop: 10, elevation: 10, justifyContent: 'center' }}>
-                  <View style={{ width: '50%', backgroundColor: 'white', height: '20%', justifyContent: 'center', alignItems: 'center', borderRadius: 30, elevation: 10, }}>
-                    <Text style={{ color: 'black' }} onPress={() => navigation.navigate(PATH_HOME.viewworkshop)}>{item.name}</Text>
-                  </View>
-                </View>
-          )
-        }) : null}
-    </>
+      <FlatList
+        showsVerticalScrollIndicator
+        data={workshop}
+        renderItem={({ item }) => (
+          <View style={{ backgroundColor: 'firebrick', alignItems: 'center', borderRadius: 50, paddingVertical: 30, marginHorizontal: 40, marginVertical: 10, elevation: 10, justifyContent: 'center', width: '80%' }}>
+            <Text style={{ color: 'black' }} onPress={() => navigation.navigate(PATH_HOME.viewworkshop, { workshopId: item?.id })}>{item.name}</Text>
+          </View>
+        )}
+        keyExtractor={item => item.id}
+      />
+    </SafeAreaView>
   );
 }
 
