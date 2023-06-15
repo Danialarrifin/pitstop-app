a78eb5802a3673a4a5890c4fac6d5dd05e159269
danial <danialarrifin11@gmail.com>
2023-06-16 01:34:45 +0800

    get all but we want it bby id, need to change

--- src/screen/Vehicle.js
+++ src/screen/Vehicle.js
@@ -1,54 +1,70 @@
-import { Button, View, Text, TextInput, Image } from 'react-native';
+import { Button, View, Text, TextInput, Image, SafeAreaView, FlatList } from 'react-native';
 import { PATH_AUTH, PATH_HOME } from '../navigations/path';
 import AntDesgin from 'react-native-vector-icons/AntDesign'
+import axiosInstance from '../utils/axios';
+import React, { useContext, useState, useEffect } from 'react';
+import { AuthContext } from '../context/AuthContext';
+
+
+
+
+
 function Vehicle({ navigation }) {
+    const [vehicle, setVehicle] = useState([]);
+    const { userInfo } = useContext(AuthContext);
+
+    useEffect(() => {
+        getAllVehicle();
+    }, []);
+
+    const getAllVehicle = async () => {
+        try {
+            const response = await axiosInstance.get(
+                "/vehicles",
+                {
+                    headers: {
+                        'Authorization': `Bearer ${userInfo?.access_token}`
+                    }
+                }
+            );
+            console.log('response vehicle', response.data);
+            if (response.data.length > 0)
+                setVehicle(response.data);
+        } catch (err) {
+            console.log(err);
+        }
+    }
     return (
-        <>
+
+        <SafeAreaView style={{ flex: 1 }}>
             <View style={{ backgroundColor: 'firebrick', borderBottomEndRadius: 4, height: '10%' }} >
                 <View style={{ flexDirection: 'row', marginVertical: 5 }}>
                     <AntDesgin name='arrowleft' style={{ color: 'white', fontSize: 20, marginTop: 20, marginLeft: 10 }} onPress={() => navigation.navigate(PATH_HOME.dashboard)} />
-                    <Text style={{ marginTop: 18, fontSize: 15, marginLeft: 115, color: 'white' }}> View My Vehicle</Text>
+                    <Text style={{ marginTop: 18, fontSize: 15, marginLeft: 145, color: 'white' }}> Vehicles</Text>
                 </View>
             </View>
-            <View style={{ backgroundColor: 'white', alignItems: 'center', borderRadius: 50, height: '25%', marginLeft: 40, marginRight: 40, marginBottom: 10, marginTop: 50, elevation: 10, justifyContent: 'center' }}>
-                {/* <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }} >
-      <Image source={require('../assets/icons/logo.png')} style={{ width: '200%', height: '200%'}}/>
-        </View>    */}
-                <View style={{ width: '30%', backgroundColor: 'white', height: '20%', justifyContent: 'center', alignItems: 'center' }}>
-                    <Text style={{ color: 'firebrick' }}>Suzuki Swift</Text>
-                </View>
-            </View>
-            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-                <View style={{ backgroundColor: "white", width: '30%', marginHorizontal: 5, borderRadius: 4, alignItems: 'center', justifyContent: 'center', elevation: 20, marginBottom: 10, }}>
-                    <View style={{ flexDirection: 'row', marginVertical: 5, }}>
-                        <Text style={{ color: 'firebrick', marginRight: 5 }}><AntDesgin name='car' style={{ color: 'firebrick', fontSize: 20 }} /></Text>
-                        <Text style={{ color: 'firebrick' }}>WWF1738</Text>
-                    </View>
-                </View>
-            </View>
-            <View style={{ backgroundColor: 'white', alignItems: 'center', borderRadius: 50, height: '25%', marginLeft: 40, marginRight: 40, marginBottom: 10, elevation: 10, justifyContent: 'center', marginTop: 10 }}>
-                <View style={{ width: '30%', backgroundColor: 'white', height: '20%', justifyContent: 'center', alignItems: 'center' }}>
-                    <Text style={{ color: 'firebrick' }}>Perodua Myvi</Text>
-                </View>
-            </View>
-            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-                <View style={{ backgroundColor: "white", width: '30%', marginHorizontal: 5, borderRadius: 4, alignItems: 'center', justifyContent: 'center', elevation: 20, marginBottom: 10, }}>
-                    <View style={{ flexDirection: 'row', marginVertical: 5, }}>
-                        <Text style={{ color: 'firebrick', marginRight: 5 }}><AntDesgin name='car' style={{ color: 'firebrick', fontSize: 20 }} /></Text>
-                        <Text style={{ color: 'firebrick' }}>VHL9808</Text>
-                    </View>
-                </View>
-            </View>
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <View style={{ width: '80%', height: '60%', }}>
-                    <Button
-                        color={'#b22222'}
-                        title="Update Vehicle"
-                        onPress={() => navigation.navigate(PATH_HOME.addvehicle)}
-                    />
-                </View>
-            </View>
-        </>
+            <FlatList
+                showsVerticalScrollIndicator
+                data={vehicle}
+                renderItem={({ item }) => (
+                    <>
+                        <View style={{ backgroundColor: 'firebrick', alignItems: 'center', borderRadius: 10, paddingVertical: 30, marginHorizontal: 40, marginVertical: 10, elevation: 10, justifyContent: 'center', width: '80%' }}>
+                            <Text style={{ color: 'white' }}>{item.manufacturer}</Text>
+                            <Text style={{ color: 'white' }}>{item.model}</Text>
+                        </View>
+                        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                            <View style={{ backgroundColor: "white", width: '30%', marginHorizontal: 5, borderRadius: 4, alignItems: 'center', justifyContent: 'center', elevation: 20, marginBottom: 10, }}>
+                                <View style={{ flexDirection: 'row', marginVertical: 5, }}>
+                                    <Text style={{ color: 'firebrick', marginRight: 5 }}><AntDesgin name='car' style={{ color: 'firebrick', fontSize: 20 }} /></Text>
+                                    <Text style={{ color: 'firebrick' }}>{item.plate_num}</Text>
+                                </View>
+                            </View>
+                        </View>
+                    </>
+                )}
+                keyExtractor={item => item.id}
+            />
+        </SafeAreaView>
     );
 }
 
