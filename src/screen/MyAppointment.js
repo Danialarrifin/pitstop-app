b47b937dfe0f39ec9f9ea7f1272f4fc14be09afd
danial <danialarrifin11@gmail.com>
2023-06-21 01:57:08 +0800

    get appointment by userID

--- src/screen/MyAppointment.js
+++ src/screen/MyAppointment.js
@@ -1,13 +1,15 @@
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, TextInput, Image, SafeAreaView, FlatList } from 'react-native';
 import { PATH_AUTH, PATH_HOME } from '../navigations/path';
 import AntDesgin from 'react-native-vector-icons/AntDesign'
+import axiosInstance from '../utils/axios';
 import React, { useContext, useState, useEffect } from 'react';
 import { AuthContext } from '../context/AuthContext';
-import axiosInstance from '../utils/axios';
 
 
-function MyAppointment({ navigation, route }) {
 
+
+
+function MyAppointment({ navigation }) {
     const [appointment, setAppointment] = useState([]);
     const { userInfo } = useContext(AuthContext);
 
@@ -17,7 +19,6 @@ function MyAppointment({ navigation, route }) {
 
     const getAppointment = async () => {
         try {
-           
             const response = await axiosInstance.get(
                 `/appointments?userId=${userInfo.user.id}`,
                 {
@@ -26,53 +27,59 @@ function MyAppointment({ navigation, route }) {
                     }
                 }
             );
-            console.log('response appointments', response.data);
-            if (response.data)
-                setAppointment(response.data[0]);
+            console.log('response MyAppointment', response.data);
+            if (response.data.length > 0)
+                setAppointment(response.data);
         } catch (err) {
             console.log(err);
         }
     }
     return (
-        <>
-        <View style={{ backgroundColor: 'firebrick',  borderBottomEndRadius: 4, height: '10%' }} >
+
+        <SafeAreaView style={{ flex: 1 }}>
+            <View style={{ backgroundColor: 'firebrick', borderBottomEndRadius: 4, height: '10%' }} >
                 <View style={{ flexDirection: 'row', marginVertical: 5 }}>
                     <AntDesgin name='arrowleft' style={{ color: 'white', fontSize: 20, marginTop: 20, marginLeft: 10 }} onPress={() => navigation.navigate(PATH_HOME.dashboard)} />
-                    <Text style={{marginTop: 18, fontSize: 15, marginLeft: 100, color: 'white'}}> View My Appointments</Text>
+                    <Text style={{ marginTop: 18, fontSize: 15, marginLeft: 120, color: 'white' }}> My Appointments</Text>
                 </View>
             </View>
-            <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: 30 }}>
-                <Text style={{ fontSize: 20, marginBottom: 10, color: 'firebrick', fontFamily: 'Roboto-Bold' }}  onPress={() => navigation.navigate(PATH_HOME.dashboard)}> My Appointments</Text>
-            </View>
-            <View style={{ backgroundColor: 'firebrick', alignItems: 'center', borderRadius: 30, height: '30%', marginLeft: 30, marginBottom: 10, elevation: 10, justifyContent: 'center', width: '85%' }}>
-                <Text style={{ fontSize: 20, marginBottom: 5, color: 'white' }}>{appointment?.workshop_name}</Text>
-                <View style={{ flexDirection: 'row', marginVertical: 5 }}>
-                    <AntDesgin name='calendar' style={{ color: 'white', fontSize: 20, marginTop: 5 }} />
-                    <Text style={{ fontSize: 20, marginBottom: 5, color: 'white', marginLeft: 20 }}>29/11/2022</Text>
-                </View>
-                <View style={{ flexDirection: 'row', marginVertical: 5, }}>
-                    <AntDesgin name='clockcircleo' style={{ color: 'white', fontSize: 20, marginTop: 5 }} />
-                    <Text style={{ fontSize: 20, marginBottom: 5, color: 'white', marginLeft: 20 }}>11.00 am - 12.30 pm</Text>
-                </View>
-                <View style={{ flexDirection: 'row', marginVertical: 5, }}>
-                    <AntDesgin name='car' style={{ color: 'white', fontSize: 20, marginTop: 5 }} />
-                    <Text style={{ fontSize: 20, marginBottom: 5, color: 'white', marginLeft: 20 }}>Brake Pad Change and Flat Tyre</Text>
-                </View>
-                <View style={{ flexDirection: 'row', marginVertical: 5, }}>
-                    <Text style={{ fontSize: 20, marginBottom: 5, color: 'white', marginLeft: 20 }}>Status:</Text>
-                    <Text style={{ fontSize: 20, marginBottom: 5, color: 'white', marginLeft: 20, fontFamily: 'Roboto-Bold' }}>Accepted</Text>
-                </View>
-            </View>
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <View style={{ width: '80%', height: '60%', }}>
-                    <Button
-                        color={'#b22222'}
-                        title="Book New Appointment"
-                    onPress={() => navigation.navigate(PATH_HOME.appointment)}
-                    />
-                </View>
-            </View>
-        </>
+            <FlatList
+                showsVerticalScrollIndicator
+                data={appointment}
+                renderItem={({ item }) => (
+                    <>
+                        <View style={{ backgroundColor: 'firebrick', alignItems: 'center', borderRadius: 10, paddingVertical: 30, marginHorizontal: 40, marginVertical: 10, elevation: 10, justifyContent: 'center', width: '80%' }}>
+                            <Text style={{ color: 'white' }}>{item.workshop_name}</Text>
+                            <View style={{ flexDirection: 'row', marginVertical: 5, }}>
+                                    <Text style={{ color: 'white', marginRight: 5 }}><AntDesgin name='setting' style={{ color: 'white', fontSize: 20 }} /></Text>
+                                    <Text style={{ color: 'white' }}>{item.service_name}</Text>
+                                </View>
+                                <View style={{ flexDirection: 'row', marginVertical: 5, }}>
+                                    <Text style={{ color: 'white', marginRight: 5 }}><AntDesgin name='clockcircleo' style={{ color: 'white', fontSize: 20 }} /></Text>
+                                    <Text style={{ color: 'white' }}>{item.start_time + '-' + item.end_time}</Text>
+                                </View>
+                                <View style={{ flexDirection: 'row', marginVertical: 5, }}>
+                                    <Text style={{ color: 'white', marginRight: 5 }}><AntDesgin name='car' style={{ color: 'white', fontSize: 20 }} /></Text>
+                                    <Text style={{ color: 'white' }}>{item.model}</Text>
+                                </View>
+                                <View style={{ flexDirection: 'row', marginVertical: 5, }}>
+                                    <Text style={{ color: 'white', marginRight: 5 }}><AntDesgin name='car' style={{ color: 'white', fontSize: 20 }} /></Text>
+                                    <Text style={{ color: 'white' }}>{item.status}</Text>
+                                </View>
+                        </View>
+                        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                            <View style={{ backgroundColor: "white", width: '30%', marginHorizontal: 5, borderRadius: 4, alignItems: 'center', justifyContent: 'center', elevation: 20, marginBottom: 10, }}>
+                                <View style={{ flexDirection: 'row', marginVertical: 5, }}>
+                                    <Text style={{ color: 'firebrick', marginRight: 5 }}><AntDesgin name='close' style={{ color: 'firebrick', fontSize: 20 }} /></Text>
+                                    <Text style={{ color: 'firebrick' }}>Cancel</Text>
+                                </View>
+                            </View>
+                        </View>
+                    </>
+                )}
+                keyExtractor={item => item.id}
+            />
+        </SafeAreaView>
     );
 }
 
