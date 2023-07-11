e16d62f528fe128474a0373ceaa155e51d6a4e43
danial <danialarrifin11@gmail.com>
2023-07-12 00:49:11 +0800

    text color black

--- src/screen/Login.js
+++ src/screen/Login.js
@@ -16,12 +16,12 @@ function Login({ navigation }) {
             <View style={{ width: '80%', height: '60%', }}>
 
                 <Text style={{ color: 'firebrick' }} >Username</Text>
-                <TextInput style={{ height: 50, width: 'auto', marginBottom: 15, borderColor: 'black', borderWidth: 1, backgroundColor: 'white', borderRadius: 4 }} value={email} onChangeText={text => setEmail(text)} />
+                <TextInput style={{ height: 50, width: 'auto', marginBottom: 15, borderColor: 'black', borderWidth: 1, backgroundColor: 'white', borderRadius: 4, color: '#000000' }} value={email} onChangeText={text => setEmail(text)} />
                 <View style={{ flexDirection: 'row', justifyContent: 'flex-end', marginTop: 10 }}>
                     <TouchableOpacity onPress={() => navigation.navigate(PATH_AUTH.forgotpassword)}><Text>Forgot your password?</Text></TouchableOpacity>
                 </View>
                 <Text style={{ color: 'firebrick' }}>Password</Text>
-                <TextInput style={{ height: 50, width: 'auto', marginBottom: 15, borderColor: 'black', borderWidth: 1, backgroundColor: 'white', borderRadius: 4 }}  value={password}   onChangeText={text => setPassword(text)}
+                <TextInput style={{ height: 50, width: 'auto', marginBottom: 15, borderColor: 'black', borderWidth: 1, backgroundColor: 'white', borderRadius: 4, color: '#000000' }}  value={password}   onChangeText={text => setPassword(text)}
           secureTextEntry/>
                 <Button
                     color={'#b22222'}
@@ -30,9 +30,9 @@ function Login({ navigation }) {
                         handleLogin()
                       }}
                 />
-                <View style={{ flexDirection: 'row', justifyContent: 'center', alignContent: 'center', marginTop: 10 }}>
-                    <Text>New User? </Text>
-                    <TouchableOpacity onPress={() => navigation.navigate(PATH_AUTH.register)}><Text>REGISTER</Text></TouchableOpacity>
+                <View style={{ flexDirection: 'row', justifyContent: 'center', alignContent: 'center', marginTop: 10, color: 'black' }}>
+                    <Text style={{ color: 'black'}}>New User? </Text>
+                    <TouchableOpacity style={{ color: 'black' }} onPress={() => navigation.navigate(PATH_AUTH.register)}><Text style={{color:'black'}}>REGISTER</Text></TouchableOpacity>
                 </View>
             </View>
         </View>
